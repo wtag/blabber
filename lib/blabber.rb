b2e311ef9af86860d7a52d698c0a14fa51c5c0c6
Junan Chakma <junan.chakma@welltravel.com>
2018-11-13 19:55:16 +0600

    remove dupplicate file

--- lib/blabber.rb
+++ lib/blabber.rb
@@ -1,6 +1,3 @@
 require 'blabber/version'
 require_relative '../ruby/agent-platform/userRelationships_services_pb.rb'
-
-class BlabberService
-  include Agent::UserRelationships::Service
-end
+require_relative '../ruby/chat-backend/chat_services_pb.rb'
--- ruby/lib/blabber.rb
+++
@@ -1,6 +0,0 @@
-require 'blabber/version'
-require_relative '../ruby/agent-platform/userRelationships_services_pb.rb'
-
-class BlabberService
-  include Agent::UserRelationships::Service
-end
