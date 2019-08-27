7d0cad1a2a331748ce0551154d086c3da55b4363
Sujon Al Mahmud <sujon.almahmud@welldev.io>
2019-08-27 16:59:02 +0600

    reverts to lowercase folder name

--- nodejs/chat-backend/chat_pb.js
+++ nodejs/chat-backend/chat_pb.js
@@ -11,7 +11,7 @@ var jspb = require('google-protobuf');
 var goog = jspb;
 var global = Function('return this')();
 
-var Model_user_pb = require('../Model/user_pb.js');
+var Model_user_pb = require('../model/user_pb.js');
 goog.object.extend(proto, Model_user_pb);
 goog.exportSymbol('proto.ChatService.AccessToken', null, global);
 goog.exportSymbol('proto.ChatService.Empty', null, global);
