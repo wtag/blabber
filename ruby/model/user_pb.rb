7820bd13b7fd296f582a493b6e3bd3edb013506f
khaled <khaled.hossain@welldev.io>
2020-06-08 20:14:11 +0600

    update build

--- ruby/model/user_pb.rb
+++ ruby/model/user_pb.rb
@@ -4,17 +4,19 @@
 require 'google/protobuf'
 
 Google::Protobuf::DescriptorPool.generated_pool.build do
-  add_message "Model.User" do
-    optional :id, :int64, 1
-    optional :name, :string, 2
-    optional :tenant, :string, 3
-    optional :isCustomer, :bool, 4
-    optional :avatarURL, :string, 5
-    optional :isOnline, :bool, 6
-    repeated :agents, :message, 7, "Model.User"
+  add_file("model/user.proto", :syntax => :proto3) do
+    add_message "Model.User" do
+      optional :id, :int64, 1
+      optional :name, :string, 2
+      optional :tenant, :string, 3
+      optional :isCustomer, :bool, 4
+      optional :avatarURL, :string, 5
+      optional :isOnline, :bool, 6
+      repeated :agents, :message, 7, "Model.User"
+    end
   end
 end
 
 module Model
-  User = Google::Protobuf::DescriptorPool.generated_pool.lookup("Model.User").msgclass
+  User = ::Google::Protobuf::DescriptorPool.generated_pool.lookup("Model.User").msgclass
 end
