7a61d3525a134a85d6dfbdb67a1f95e3d0c33313
khaled <khaled.hossain@welldev.io>
2020-06-09 10:44:12 +0600

    fix indent

--- ruby/agent-platform/userRelationships_pb.rb
+++ ruby/agent-platform/userRelationships_pb.rb
@@ -5,16 +5,16 @@ require 'google/protobuf'
 
 require 'model/user_pb'
 Google::Protobuf::DescriptorPool.generated_pool.build do
-    add_message "Agent.UserRelationsCustomer" do
-      repeated :agents, :message, 1, "Model.User"
-      repeated :travelArrangers, :message, 2, "Model.User"
-      repeated :colleagues, :message, 3, "Model.User"
-      repeated :travelArrangementClients, :message, 4, "Model.User"
-    end
-    add_message "Agent.UserRelationsAgent" do
-      repeated :customers, :message, 1, "Model.User"
-      repeated :colleagues, :message, 2, "Model.User"
-    end
+  add_message "Agent.UserRelationsCustomer" do
+    repeated :agents, :message, 1, "Model.User"
+    repeated :travelArrangers, :message, 2, "Model.User"
+    repeated :colleagues, :message, 3, "Model.User"
+    repeated :travelArrangementClients, :message, 4, "Model.User"
+  end
+  add_message "Agent.UserRelationsAgent" do
+    repeated :customers, :message, 1, "Model.User"
+    repeated :colleagues, :message, 2, "Model.User"
+  end
 end
 
 module Agent
--- ruby/model/user_pb.rb
+++ ruby/model/user_pb.rb
@@ -4,14 +4,14 @@
 require 'google/protobuf'
 
 Google::Protobuf::DescriptorPool.generated_pool.build do
-    add_message "Model.User" do
-      optional :id, :int64, 1
-      optional :name, :string, 2
-      optional :tenant, :string, 3
-      optional :isCustomer, :bool, 4
-      optional :avatarURL, :string, 5
-      optional :isOnline, :bool, 6
-      repeated :agents, :message, 7, "Model.User"
+  add_message "Model.User" do
+    optional :id, :int64, 1
+    optional :name, :string, 2
+    optional :tenant, :string, 3
+    optional :isCustomer, :bool, 4
+    optional :avatarURL, :string, 5
+    optional :isOnline, :bool, 6
+    repeated :agents, :message, 7, "Model.User"
   end
 end
 
