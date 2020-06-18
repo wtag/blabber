79d57d4902bfd9edcd19b56e64bb6c36e9f043c6
khaled <khaled.hossain@welldev.io>
2020-06-18 10:31:00 +0600

    make retrieveAvailableRooms stream

--- ruby/chat-backend/chat_services_pb.rb
+++ ruby/chat-backend/chat_services_pb.rb
@@ -16,7 +16,7 @@ module ChatService
 
       rpc :RetrieveMessages, Room, MessageList
       rpc :SendMessage, Message, SendMessageResponse
-      rpc :RetrieveAvailableRooms, Model::User, RoomCategoryList
+      rpc :RetrieveAvailableRooms, Model::User, stream(RoomCategory)
       rpc :MarkMessageAsSeen, RemoveMessageSeenRequest, RemoveMentionAndSeenResponse
       rpc :MarkMentionAsSeen, RemoveMentionRequest, RemoveMentionAndSeenResponse
     end
